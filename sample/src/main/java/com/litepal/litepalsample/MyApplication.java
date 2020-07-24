cd0ec63852dfdf85673fce51a02a91a3a803ac37
lqx <herolqx@126.com>
2020-07-24 15:40:02 +0800

    feat:更改包名

---
+++ sample/src/main/java/com/litepal/litepalsample/MyApplication.java
@@ -0,0 +1,14 @@
+package com.litepal.litepalsample;
+
+import android.app.Application;
+
+import com.litepal.Operator;
+
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Operator.initialize(this);
+    }
+}
